9f1c3f90d578dba22ccbbd4f5848732fe0cb4ee3
Wqntedpw <wqnted@qmail.pm>
2020-01-23 18:30:55 +0100

    Initial commit

---
+++ adduser.ps1
@@ -0,0 +1,114 @@
+﻿<#
+.NAME
+LA-SUPER-M0UL1N3TT3
+
+.AUTHOR
+Enzo
+
+.DESCRIPTION
+Ce script importe des infos depuis un CSV pour :
+    - Créer un utilisateur avec un mdp temporaire
+    - Lui envoi un email avec son id et le mot de passe temporaire
+    - Créer un repertoire dans partage
+
+.SCOPE
+Serveur AD
+
+.NOTES
+    Le script principale se trouve dans la fonction adduserad().
+
+    A voir:
+        - Le script crée le groupe si il n'existe pas
+#>
+
+# Variables
+$csv = import-csv -path "C:\Users\Administrateur\Documents\users.csv" -delimiter ";"
+$mailsecurestring = "C:\Users\Administrateur\Documents\mailpassword.xml"
+$logpath="C:\Users\Administrateur\Documents\"
+$logfile = "adduser-$(get-date -UFormat "%d-%m-%y_%H-%M-%S").log"
+$domain="nsa.local"
+$GlobalPendingOU="Pending"
+$UsersPengingOU="Users"
+$GroupPendingOU="Groups"
+$ShareRepertory="C:\Partage\Users"
+
+new-item -Path $logpath -ItemType File -Name $logfile
+
+# Generation de caracteres
+function get-randomstring () {
+    param (
+        [int]$Length,
+        $type
+    )
+    if ($type -eq "alpha") { $set = "abcdefghijklmnopqrstuvwxyz0123456789#$".ToCharArray() } elseif ($type -eq "num") { $set = "1234567890".ToCharArray() } else { exit }
+    $result = ""
+    for ($x = 0; $x -lt $Length; $x++) {
+       $result += $set | Get-Random
+    }
+    return $result
+}
+
+
+# Envoi de mail
+function sendmail () {
+    param (
+        $emailext
+    )
+    $mailuser = "devlab@qmail.pm"
+    $mailpassword = Get-Content -Path $mailsecurestring | ConvertTo-SecureString
+    $mailcredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $mailuser, $mailpassword
+    $mailbody = “<p><img src='https://i.imgur.com/1UrGu3V.png?2'> <h2>Création de compte</h2> </p><br><p>Bonjour $displayname ! <br><br>Voici les informations sur votre compte BND:<br>Nom d'utilisateur: $samname <br>Mot de passe temporaire: $pass <br></p><p style='text-color: red;'>Un nouveau mot de passe vous sera demandé lors de votre première connexion</p>”
+
+    Send-MailMessage -To “starrcsgo@gmail.com” -From $mailuser -Subject “Création de votre compte BND” -Body $mailbody -Credential $mailcredential -SmtpServer “ns.noroute.pw” -Port 587 -UseSsl -BodyAsHtml -Encoding UTF8
+}
+
+# Ajout-Creation de groupe
+function addusergroup () {
+    if (-not (Get-ADGroup -Filter "Name -eq '$group' -and GroupCategory -eq 'Security'")){
+        New-ADGroup -Name "$group" -Displayname "$group" -Path "OU=$GroupPendingOU,OU=$GlobalPendingOU,DC=nsa,DC=local" -GroupCategory Security -GroupScope Global
+    }
+
+    Add-ADGroupMember -Identity "$group" -Members "$samname"
+}
+
+# Création du repertoire personnel
+function createrepertory () {
+    if (-not (Get-Item -Path "${ShareRepertory}\${samname}")) {
+        New-Item -ItemType Directory -Path "$ShareRepertory" -Name "$samname"
+    }
+}
+
+# Ajout de l'utilisateur
+function adduserad () {
+    foreach ($user in $csv)
+    {
+        # Recuperation des infos du CSV
+        $pass = get-randomstring 16 alpha
+        $nom = $user.lastname
+        $prenom = $user.firstname
+        $ou = $user.ou
+        $group = $user.group
+        $emailext = $user.emailext
+        $displayname = (-join ("$prenom"," ","$nom"))
+        $samname = (-join ("$prenom",".","$nom")).ToLower()
+
+        # Verif si un utilisateur existant n'a pas le meme nom
+        while (get-aduser -Filter "UserPrincipalName -eq '${samname}@${domain}' -or Samaccountname -eq '${samname}'") {
+            write-host "[DEBUG]" "Le compte $samname existe"
+            $r = get-randomstring 3 num
+            $samname = (-join ("$samname","$r"))
+            $displayname = (-join ("$prenom"," ","$nom"," ","$r"))
+        }
+
+        write-host "[DEBUG]" " Nom : $nom , Prenom : $prenom , OU : $ou , SAM : $samname , Pass : $pass"
+        # Création de l'utilisateur 
+        New-ADuser -surname $nom -name $displayname -SamAccountName $samname -UserPrincipalName ${samname}@${domain} -givenname $prenom -displayname $displayname -Path $ou -ChangePasswordAtLogon $true -PasswordNeverExpires $false -AccountPassword (ConvertTo-SecureString -AsPlainText $pass -Force) -Enabled $true
+        # Envoi du mail
+        sendmail $emailext
+        # Ajout de l'utilisateur dans son groupe
+        addusergroup
+     }
+}
+
+adduserad
+
